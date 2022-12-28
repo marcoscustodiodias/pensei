6f72b84b973f5a9e44057ac5384de990f604d2fe
marcoscustodiodias <marcosdias.pvai@gmail.com>
2022-12-28 15:48:07 -0300

    Funções em Javascript criadas no projeto.

--- js/principal.js
+++ js/principal.js
@@ -8,3 +8,43 @@ const getPosts = async (param) => {
             return response.json();
         
     }
+
+    const herosfromfedd = heros.data.results.map(item =>`<li class="card ${'normal'}>
+    <li class="card ${normal`}">
+    <img class="card-image" alt=${item.name}
+    src= "${item.thumbnail.path}${'.'}{item.thubnail.extension}"/>
+    <h2 class="card-title">${item.name}</h2>
+    </li    >
+    `).join('')
+
+
+//Exibe os Herois iniciais:
+const earlyFeed = async () => {
+    const heros = await getPosts('orderByname&limite=20');
+    const postsTemplate = herosFromFedd(heros)
+    ul.innerHTML = postsTemplate;
+
+}
+
+//Exibe os Herois pesquisados:
+const searchPersonIntoDOM = async (search) => {
+    const heros = await getPosts(`${ 'name=' }${search}`);
+    const postsTemplate = herosFromSearch(heros)
+    ul.innerHTML = postsTemplate;
+}
+
+//Funcao que verifica o imput "pesquisar personagens"
+const modifyimputValue = event => {
+    const inputValue = event.target.value.toLowerCase();
+    if (inputValue != '') {
+        searchPersonIntoDOM(inputValue);
+    } else if (inputValue = '' || inputValue = null) {
+        earlyFeed();
+        }
+    }
+
+    //Para preenchermos o feed inicial:
+    earlyFeed();
+    
+    //Para verificar se ocorre algum input:
+    filterImput.addEventListener('input', modifyInputValue)
